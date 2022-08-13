2d5b91a6e97f7854d671f43f92f6613b60662db1
Git <3035184396@qq.com>
2022-08-13 10:49:00 +0800

    123

--- 课时01作业/src/dzh/Test01.java
+++
@@ -1,9 +0,0 @@
-package dzh;
-
-public class Test01 {
-    public static void main(String[] args) {
-        int Txiu=245,wqx=570,wqp=320;
-        double sum = Txiu*2 + wqx+ wqp;
-        System.out.println("消费总金额:"+sum);
-    }
-}
